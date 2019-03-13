15512b4a34329fe8ebfba41218e8863622a45dec
Patrick Heflin <patrickslx@yahoo.com>
2019-03-13 09:59:43 -0500

    styling change

--- client/src/Components/Facebook.js
+++ client/src/Components/Facebook.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import FacebookLogin from 'react-facebook-login'
 import { Redirect } from 'react-router'
 
+import '../App.css'
+
 export default class Facebook extends Component {
 
   render() {
@@ -11,20 +13,20 @@ export default class Facebook extends Component {
       return <Redirect to='/input' />
     } else {
       fbContent = (
-      	<div className='Intro'>
-      			<div className='IntroElements'>
-      				<h1>PIZZA DOUGH</h1>
-      		
-			        <FacebookLogin
-			          appId='655517171497976'
-			          size='small'
-			          autoLoad={false}
-			          fields='name,email,picture'			          
-			          callback={this.props.responseFacebook}
-                isMobile={true}
-                // disableMobileRedirect={true}
-			        />
-		        </div>
+        <div className='Intro'>
+          <div className='IntroElements'>
+            <h1>PIZZA DOUGH</h1>
+
+            <FacebookLogin
+              appId='655517171497976'
+              size='small'
+              autoLoad={false}
+              fields='name,email,picture'
+              callback={this.props.responseFacebook}
+              isMobile={true}
+            // disableMobileRedirect={true}
+            />
+          </div>
         </div>
       )
     }
